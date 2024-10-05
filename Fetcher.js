a6fffc7fa527b09731ca0f9f7d505df18353dc1b
manuelmsni <manuelmsni@hotmail.com>
2024-10-05 21:14:14 +0200

    Add files via upload

---
+++ Fetcher.js
@@ -0,0 +1,95 @@
+class Fetcher {
+    requiresCorsProxy = false;
+    initialized = false;
+
+    constructor() {
+        this.initialize();
+    }
+
+    async initialize() {
+        try {
+            await this.testCors();
+            this.initialized = true;
+            console.log('Fetcher initialized');
+        } catch (error) {
+            console.error('Failed to initialize Fetcher:', error);
+        }
+    }
+
+    async waitForInitialization() {
+        while (!this.initialized) {
+            await new Promise(resolve => setTimeout(resolve, 10));
+        }
+    }
+
+    async testCors() {
+        var response;
+        try {
+            response = await fetch('https://httpbin.org/cors');
+        } catch (error) {
+            console.log('Error al cargar el documento:', error);
+        } finally {
+            if (!response || !response.ok) {
+                this.requiresCorsProxy = true;
+                console.log('Proxy required for CORS requests.');
+            }
+        }
+    }
+
+    async fetchFileContent(url) {
+        await this.waitForInitialization();
+        const response = await fetch(url);
+        const data = await response.text();
+        return data;
+    }
+
+    async fetchFileContentAvoidingCors(url) {
+        await this.waitForInitialization();
+        var data;
+        try {
+            if (!this.requiresCorsProxy) {
+                data = await this.fetchFileContent(url);
+            } else {
+                data = await this.fetchFileContent('https://corsproxy.io/?' + url);
+            }
+        } catch (error) {
+            console.error('Error al cargar el documento:', error);
+        } finally {
+            return data;
+        }
+    }
+
+    async fetchGoogleSheetsCSV(sheetId, sheetGID) {
+        await this.waitForInitialization();
+        var targetUrl = `https://docs.google.com/spreadsheets/d/${sheetId}/export?format=csv&gid=${sheetGID}`;
+        var csv;
+        try {
+            csv = await this.fetchFileContentAvoidingCors(targetUrl);
+            const rows = csv.split('\n').map(row => row.trim());
+            const array2D = rows.map(row => row.split(',').map(col => col.trim().replace("\r", "")));
+            return array2D;
+        } catch (error) {
+            console.error('Error al cargar el csv:', error);
+        }
+    }
+
+    async fetchGoogleSheetsCSVAsJson(sheetId, sheetGID) {
+        const array2D = await this.fetchGoogleSheetsCSV(sheetId, sheetGID);
+
+        if (!array2D || array2D.length < 2) {
+            console.error("CSV no tiene suficientes datos.");
+            return [];
+        }
+
+        const [headers, ...rows] = array2D;
+        const jsonArray = rows.map(row => {
+            let obj = {};
+            headers.forEach((header, index) => {
+                obj[header] = row[index];
+            });
+            return obj;
+        });
+
+        return jsonArray;
+    }
+}
