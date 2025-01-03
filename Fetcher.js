06c28b1157e6260a91c1dfc770f265a0b709045e
manuelmsni <manuelmsni@hotmail.com>
2025-01-03 19:32:00 +0100

    Update Fetcher.js

--- Fetcher.js
+++ Fetcher.js
@@ -124,51 +124,61 @@ class Fetcher {
     }
 
     async fetchGoogleDocsHtml(docId) {
-	const text = await this.fetchGoogleDocsPlainText(docId);
-        const lines = text.split('\n');
-        let html = '';
-        let isInList = false;
-        let listType = null;
-
-        lines.forEach(line => {
+        const text = await this.fetchGoogleDocsPlainText(docId);
+	const lines = text.split('\n');
+	let html = '';
+	let isInList = false;
+	let listType = null;
+	
+	lines.forEach(line => {
 	    const trimmedLine = line.trim();
-
-	    if (trimmedLine.startsWith('*')) {
+	    const headerMatch = /^#{1,6}\s/.exec(trimmedLine);
+	    if (headerMatch) {
+	        if (isInList) {
+	            html += `</${listType}>`;
+	            isInList = false;
+	            listType = null;
+	        }
+	        const level = headerMatch[0].trim().length;
+	        html += `<h${level}>${trimmedLine.slice(level).trim()}</h${level}>`;
+	    }
+	
+	    else if (trimmedLine.startsWith('*')) {
 	        if (!isInList || listType !== 'ul') {
-		    if (isInList) html += `</${listType}>`;
-		    html += '<ul>';
-		    isInList = true;
-		    listType = 'ul';
+	            if (isInList) html += `</${listType}>`;
+	            html += '<ul>';
+	            isInList = true;
+	            listType = 'ul';
 	        }
 	        html += `<li>${trimmedLine.slice(1).trim()}</li>`;
 	    }
-
+	
 	    else if (/^\d+\./.test(trimmedLine)) {
 	        if (!isInList || listType !== 'ol') {
-		    if (isInList) html += `</${listType}>`;
-		    html += '<ol>';
-		    isInList = true;
-		    listType = 'ol';
+	            if (isInList) html += `</${listType}>`;
+	            html += '<ol>';
+	            isInList = true;
+	            listType = 'ol';
 	        }
 	        html += `<li>${trimmedLine.replace(/^\d+\.\s*/, '').trim()}</li>`;
 	    }
-
+	
 	    else {
 	        if (isInList) {
-		    html += `</${listType}>`;
-		    isInList = false;
-		    listType = null;
+	            html += `</${listType}>`;
+	            isInList = false;
+	            listType = null;
 	        }
 	        if (trimmedLine) {
-		    html += `<p>${trimmedLine}</p>`;
+	            html += `<p>${trimmedLine}</p>`;
 	        }
 	    }
-        });
-
+	});
+	
         if (isInList) {
 	    html += `</${listType}>`;
-        }
-	    
-        return html;
+	}
+	
+	return html;
     }
 }
